f4a394a92d833fc3d3e84859cedf2f7af91d2705
Ayoub Aharmim <aharmim.ayoub@outlook.com>
2021-08-02 17:45:20 +0100

    Add reuable colors as constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+    BLACK: '#252A31',
+    BLUE: '#006CFF',
+    PURPLE: '#B678FF',
+    YELLOW: '#FFE761',
+    RED: '#F45E6D',
+    GREEN: '#61DEA4',
+    GREY: '#EBEFF5'
+}
